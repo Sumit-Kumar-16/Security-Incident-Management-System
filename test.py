f00787c4394fcbcdc75b2807a8e3063e2bb02264
Sumit Kumar <72264135+Sumit-Kumar-16@users.noreply.github.com>
2026-09-16 03:56:53 +0530

    test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-
-password =input("Enter your password which you want to decrypt: ")
-
-decrypt =generate_password_hash(password)
-
-print(decrypt)
-
